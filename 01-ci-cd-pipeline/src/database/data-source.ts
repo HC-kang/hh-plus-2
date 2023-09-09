ae27a302634c947c5d26b81a7a2136f0140fdbdc
HC-kang <weston0713@gmail.com>
2023-09-10 00:06:55 +0900

    Simplify

--- 01-ci-cd-pipeline/src/database/data-source.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-import { DataSource } from 'typeorm';
-import databaseConfig from '@/config/database.config';
-
-config({ path: '.env.development' });
-
-const dbConfig = databaseConfig() as any;
-export default new DataSource(dbConfig);
