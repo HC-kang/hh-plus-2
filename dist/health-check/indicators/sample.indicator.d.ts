1868b1683c028be745120a97504c5036b6aff051
HC-kang <weston0713@gmail.com>
2023-09-13 21:08:19 +0900

    Chore: image without database

--- dist/health-check/indicators/sample.indicator.d.ts
+++
@@ -1,9 +0,0 @@
-import { HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus';
-export interface Sample {
-    name: string;
-    type: string;
-}
-export declare class SampleHealthIndicator extends HealthIndicator {
-    private nodes;
-    isHealthy(key: string): Promise<HealthIndicatorResult>;
-}
